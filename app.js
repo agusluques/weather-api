664b50edfb31e97bc73d6bf6865cd1ee1876126d
Agustin <agus.luques@hotmail.com>
2018-08-28 13:42:19 -0300

    implemento llamado a api con la city id, agrego listado de ciudades

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 var express = require('express');
+var https = require('https');
+
 var app = express();
 
 const API_KEY = "5516ab386ad112c122c857849a9555a3";
+
 
 app.get('/', function (req, res) {
   	res.send('Hello World!');
 });
 
-app.get('/cities', function(req, res){
+app.get('/weather/:city_id', function(req, res){
+	var options = {
+		host :  'api.openweathermap.org',
+		port : 443,
+		path : '/data/2.5/forecast?id=' + req.params.city_id + '&APPID=' + API_KEY + '&units=metric',
+		method : 'GET'
+	}
 
+	var getReq = https.request(options, function(result) {
+		    
+	    result.on('data', function(data) {
+	        res.send( JSON.parse(data) );
+	    });
+	});
+
+	getReq.end();
+	getReq.on('error', function(err){
+		console.log("Error: ", err);
+	});	
+})
+
+app.get('/cities', function(req, res){
+	res.send('Todas las ciudades');
 })
 
 app.listen(3000, function () {
